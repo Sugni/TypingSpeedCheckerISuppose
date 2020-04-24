5013b6a3688c8fb23bc21592f3b640ba7de0f8fe
Ingus Andris Pērkons <slaimy333@gmail.com>
2020-04-24 17:35:09 +0300

    This is as finished as i think it will be. It doesn't have diffrent texts and the given text is short but other than that it work fine. Oh well another maymay done i suppose.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,9 +28,15 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.lablMainText = new System.Windows.Forms.Label();
             this.tboxWritingSpace = new System.Windows.Forms.TextBox();
             this.pboxClose = new System.Windows.Forms.PictureBox();
+            this.pbarTextProgress = new System.Windows.Forms.ProgressBar();
+            this.timerSeconds = new System.Windows.Forms.Timer(this.components);
+            this.lablTime = new System.Windows.Forms.Label();
+            this.lablCPS = new System.Windows.Forms.Label();
+            this.lablCPM = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pboxClose)).BeginInit();
             this.SuspendLayout();
             // 
@@ -46,6 +52,7 @@
             // 
             // tboxWritingSpace
             // 
+            this.tboxWritingSpace.BackColor = System.Drawing.SystemColors.Window;
             this.tboxWritingSpace.Location = new System.Drawing.Point(12, 109);
             this.tboxWritingSpace.Multiline = true;
             this.tboxWritingSpace.Name = "tboxWritingSpace";
@@ -68,17 +75,60 @@
             this.pboxClose.MouseEnter += new System.EventHandler(this.pboxClose_MouseEnter);
             this.pboxClose.MouseLeave += new System.EventHandler(this.pboxClose_MouseLeave);
             // 
+            // pbarTextProgress
+            // 
+            this.pbarTextProgress.Location = new System.Drawing.Point(12, 264);
+            this.pbarTextProgress.Name = "pbarTextProgress";
+            this.pbarTextProgress.Size = new System.Drawing.Size(776, 23);
+            this.pbarTextProgress.TabIndex = 3;
+            // 
+            // timerSeconds
+            // 
+            this.timerSeconds.Interval = 1000;
+            this.timerSeconds.Tick += new System.EventHandler(this.timerSeconds_Tick);
+            // 
+            // lablTime
+            // 
+            this.lablTime.AutoSize = true;
+            this.lablTime.Location = new System.Drawing.Point(15, 325);
+            this.lablTime.Name = "lablTime";
+            this.lablTime.Size = new System.Drawing.Size(103, 17);
+            this.lablTime.TabIndex = 4;
+            this.lablTime.Text = "Time: 00:00:00";
+            // 
+            // lablCPS
+            // 
+            this.lablCPS.AutoSize = true;
+            this.lablCPS.Location = new System.Drawing.Point(15, 373);
+            this.lablCPS.Name = "lablCPS";
+            this.lablCPS.Size = new System.Drawing.Size(51, 17);
+            this.lablCPS.TabIndex = 5;
+            this.lablCPS.Text = "CPS: 0";
+            // 
+            // lablCPM
+            // 
+            this.lablCPM.AutoSize = true;
+            this.lablCPM.Location = new System.Drawing.Point(15, 399);
+            this.lablCPM.Name = "lablCPM";
+            this.lablCPM.Size = new System.Drawing.Size(53, 17);
+            this.lablCPM.TabIndex = 6;
+            this.lablCPM.Text = "CPM: 0";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.ControlBox = false;
+            this.Controls.Add(this.lablCPM);
+            this.Controls.Add(this.lablCPS);
+            this.Controls.Add(this.lablTime);
+            this.Controls.Add(this.pbarTextProgress);
             this.Controls.Add(this.pboxClose);
             this.Controls.Add(this.tboxWritingSpace);
             this.Controls.Add(this.lablMainText);
             this.Name = "Form1";
-            this.Text = "How Can YOU Type?!";
+            this.Text = "How Fast Can YOU Type?!";
             ((System.ComponentModel.ISupportInitialize)(this.pboxClose)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -90,6 +140,11 @@
         private System.Windows.Forms.Label lablMainText;
         private System.Windows.Forms.TextBox tboxWritingSpace;
         private System.Windows.Forms.PictureBox pboxClose;
+        private System.Windows.Forms.ProgressBar pbarTextProgress;
+        private System.Windows.Forms.Timer timerSeconds;
+        private System.Windows.Forms.Label lablTime;
+        private System.Windows.Forms.Label lablCPS;
+        private System.Windows.Forms.Label lablCPM;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,27 @@ namespace TypingSpeedCheckerISuppose
 {
     public partial class Form1 : Form
     {
+
+        int seconds = 0;
+        int minutes = 0;
+        int hours = 0;
+        int totalSeconds = 0;
+
         public Form1()
         {
             InitializeComponent();
+            InitializeProgressBar();
+            InitializeTimer();
+        }
+
+        private void InitializeTimer()
+        {
+            timerSeconds.Interval = 1000;
+        }
+
+        private void InitializeProgressBar()
+        {
+            pbarTextProgress.Maximum = lablMainText.Text.Length;
         }
 
         private void pboxClose_MouseEnter(object sender, EventArgs e)
@@ -34,20 +52,117 @@ namespace TypingSpeedCheckerISuppose
 
         private void tboxWritingSpace_TextChanged(object sender, EventArgs e)
         {
+            if(!timerSeconds.Enabled)
+            {
+                timerSeconds.Start();
+            }
             int textCount = tboxWritingSpace.Text.Length;
             string lablString = lablMainText.Text.Substring(0, textCount);
             if(tboxWritingSpace.Text != lablString)
             {
                 tboxWritingSpace.MaxLength = textCount;
+                tboxWritingSpace.ForeColor = Color.Red;
             }
             else
             {
                 tboxWritingSpace.MaxLength = lablMainText.Text.Length;
+                tboxWritingSpace.ForeColor = Color.FromName("WindowText");
+                UpdateProgressBar();
             }
             if(tboxWritingSpace.Text == lablMainText.Text)
             {
-                MessageBox.Show("You did it! Good job!");
+                Victory();
             }
         }
+
+        private void UpdateLabelCPS()
+        {
+            if(totalSeconds> 0)
+            {
+                lablCPS.Text = "CPS: " + CharactersPerSecond().ToString();
+            }
+        }
+
+        private void UpdateLabelCPM()
+        {
+            if (totalSeconds > 0)
+            {
+                lablCPM.Text = "CPM: " + CharactersPerMinute().ToString();
+            }
+        }
+
+        private void Victory()
+        {
+            MessageBox.Show("You did it! Good job! Letters Per Second: " + CharactersPerSecond().ToString() + "! Letters Per Minute: " + CharactersPerMinute().ToString() + "! Final Time: " + hours.ToString("00") + ":" + minutes.ToString("00") + ":" + seconds.ToString("00") + "!");
+            tboxWritingSpace.Clear();
+            timerSeconds.Stop();
+            seconds = 0;
+            hours = 0;
+            minutes = 0;
+            totalSeconds = 0;
+            UpdateTimeLabel();
+            tboxWritingSpace.MaxLength = lablMainText.Text.Length;
+            tboxWritingSpace.ForeColor = Color.FromName("WindowText");
+            UpdateProgressBar();
+        }
+
+        private decimal CharactersPerSecond()
+        {
+            return tboxWritingSpace.Text.Length/ totalSeconds;
+        }
+
+        private decimal CharactersPerMinute()
+        {
+            return CharactersPerSecond() * 60;
+        }
+
+        private void UpdateProgressBar()
+        {
+            pbarTextProgress.Value = tboxWritingSpace.Text.Length;
+        }
+
+        private void timerSeconds_Tick(object sender, EventArgs e)
+        {
+            seconds++;
+            if(seconds == 60)
+            {
+                seconds = 0;
+                minutes++;
+                if(minutes == 60)
+                {
+                    minutes = 0;
+                    hours++;
+                    if(hours > 99)
+                    {
+                        TimesUp();
+                    }
+                }
+            }
+            UpdateLabelCPS();
+            UpdateLabelCPM();
+            UpdateTimeLabel();
+            totalSeconds++;
+        }
+        
+        private void TimesUp()
+        {
+            MessageBox.Show("Times Up! Why did it take you over 99 hours? Did you really want to see what happens? Well this is all you get!");
+            timerSeconds.Stop();
+            tboxWritingSpace.Clear();
+            seconds = 0;
+            hours = 0;
+            minutes = 0;
+            totalSeconds = 0;
+            UpdateTimeLabel();
+            tboxWritingSpace.MaxLength = lablMainText.Text.Length;
+            tboxWritingSpace.ForeColor = Color.FromName("WindowText");
+            UpdateProgressBar();
+        }
+
+        private void UpdateTimeLabel()
+        {
+            lablTime.Text = "Time: " + hours.ToString("00") + ":" + minutes.ToString("00") + ":" + seconds.ToString("00");
+        }
     }
+
 }
